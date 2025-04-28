c5e9ac3cf6e6d86649ca4884cd8ff8f090ff9c57
dale <dale@bitunix.io>
2025-04-28 19:27:55 +0800

    替换国内源

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,8 +14,12 @@
  * limitations under the License.
  */
 
-pluginManagement {
+ pluginManagement {
     repositories {
+        // 阿里镜像
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
         gradlePluginPortal()
         google()
         mavenCentral()
@@ -24,6 +28,10 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // 阿里镜像
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
         google()
         mavenCentral()
     }
